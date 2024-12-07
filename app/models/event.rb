4d13b537dca808ef4d9a2e8f8cde80cdac81ba0d
Nicholas Page <nicholas.page@goat.com>
2024-12-07 14:06:29 -0800

    lint

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  has_many :event_contacts, :inverse_of => :event
+  has_many :event_contacts, inverse_of: :event
 
   accepts_nested_attributes_for :event_contacts, reject_if: :all_blank, allow_destroy: true
 
--- app/models/event_contact.rb
+++ app/models/event_contact.rb
@@ -1,3 +1,3 @@
 class EventContact < ApplicationRecord
-  belongs_to :event, :inverse_of => :event_contacts
+  belongs_to :event, inverse_of: :event_contacts
 end
