e6691bdb06ea21b155e3085083fb1b7e8f0b29e8
Nicholas Page <nicholas.page@goat.com>
2024-12-07 16:24:35 -0800

    more

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
   before_action :resume_session, only: [ :home ]
+  before_action :get_event
 
   def home
+  end
+
+  private
+
+  def get_event
+    @event = Event.last
   end
 end
